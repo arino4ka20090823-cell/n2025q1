e2e1454ea3eb02be12105e5665ab266432fdd2b5
Arina Satsiuk <arino4ka20090823@gmail.com>
2025-10-31 15:06:25 +0300

    задачи на 31 окт

---
+++ bearAndWolf.js
@@ -0,0 +1,14 @@
+const animals=['1 bear', '2 wolves', '1 bear', '1 bear', '2 wolves', '2 wolves', '1 bear', '2 wolves',
+    '1 bear', '2 wolves', '1 bear', '1 bear', '2 wolves', '1 bear', '2 wolves', '1 bear',
+    '2 wolves', '2 wolves', '1 bear', '1 bear', '2 wolves', '1 bear', '2 wolves', '2 wolves',
+    '1 bear', '1 bear', '2 wolves', '1 bear', '2 wolves', '1 bear', '2 wolves', '1 bear',
+    '2 wolves', '1 bear', '2 wolves', '1 bear', '1 bear', '2 wolves', '2 wolves', '1 bear',
+    '2 wolves', '1 bear', '1 bear', '2 wolves', '1 bear', '2 wolves', '2 wolves', '2 wolves',
+    '1 bear', '1 bear', '2 wolves', '1 bear', '2 wolves', '1 bear', '2 wolves', '1 bear',
+    '2 wolves', '1 bear', '1 bear', '2 wolves', '1 bear', '2 wolves', '2 wolves', '1 bear',
+    '1 bear', '1 bear', '2 wolves', '2 wolves', '2 wolves', '1 bear', '2 wolves', '1 bear',
+    '1 bear', '1 bear', '2 wolves', '2 wolves', '1 bear', '2 wolves', '1 bear', '1 bear'
+]
+console.log (animals.length)
+
+console.log(animals.filter(a=> a==='1 bear' ).length)
---
+++ word.js
@@ -0,0 +1,6 @@
+const word='свекрында'
+const firstletter=word.slice(0,1).toUpperCase()
+console.log (firstletter)
+const otherLetter = word.slice(1,word.length)
+console.log (firstletter+otherLetter)
+
